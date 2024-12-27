27d80d1608e9539ed64345f0dcd6dcea4d0632ee
Linus <liinus.wiberg@gmail.com>
2024-12-27 21:59:26 +0200

    Feat: Started on guild preparation

--- src/discord/mod.rs
+++ src/discord/mod.rs
@@ -1,21 +1,40 @@
 use std::collections::HashMap;
 use std::time::Duration;
-use serenity::all::{Context, EventHandler, Message, Ready, Guild, UnavailableGuild, RoleId, Role, EditRole};
+use serenity::all::{Context, EventHandler, Message, Ready, Guild, UnavailableGuild, RoleId, Role, EditRole, GuildId};
+use serenity::model::Colour;
 use serenity::async_trait;
 use tokio::time::sleep;
 use tracing::{error, info};
 
-pub struct DiscordBot;
+pub struct DiscordBot{
+    prepared_guilds: HashMap<GuildId, HashMap<&'static str, RoleId>>,
+}
 
 #[async_trait]
 impl EventHandler for DiscordBot {
-    async fn guild_create(&self, ctx: Context, guild: Guild, is_new: Option<bool>) {
+    async fn guild_create(&mut self, ctx: Context, guild: Guild, is_new: Option<bool>) {
+
         info!("Connection to guild '{}' established!", guild.name);
 
-        prepare_guild(ctx, guild).await;
+        if !self.prepared_guilds.contains_key(&guild.id) {
+            info!("Guild already prepared!", guild.name);
+            return;
+        }
+
+        let guild_roles = prepare_guild(ctx, &guild).await;
+
+        match guild_roles {
+            Some(value) => {
+                self.prepared_guilds.insert(guild.id, value);
+            },
+            None() => {
+                error!("Failed to prepare guild '{}'", guild.name)
+            },
+        }
+
     }
 
-    async fn guild_delete(&self, ctx: Context, incomplete: UnavailableGuild, full: Option<Guild>) {
+    async fn guild_delete(&mut self, ctx: Context, incomplete: UnavailableGuild, full: Option<Guild>) {
 
         let guild_identifier;
 
@@ -44,30 +63,36 @@ impl EventHandler for DiscordBot {
 
 
     async fn ready(&self, _: Context, ready: Ready) {
+
+        DiscordBot {
+            prepared_guilds: HashMap::new(),
+        };
+
         info!("{} is connected!", ready.user.name);
     }
+
 }
 
-async fn prepare_guild(ctx: Context, guild: Guild){
+async fn prepare_guild(ctx: Context, guild: &Guild) -> Option<HashMap<&'static str, RoleId>> {
 
     info!("Preparing guild '{}' with ID '{}'!", guild.name, guild.id);
 
     if guild.id != 1322261733053825086 {
         info!("Testing mode on, returning.");
-        return;
+        return None;
     }
 
-    let required_roles: Vec<(&str, &str)> = vec![
-        ("Level 1 (1-800 ELO)", "#dddddd"),
-        ("Level 2 (801-950 ELO)", "#47e36e"),
-        ("Level 3 (951-1100 ELO)", "#47e36e"),
-        ("Level 4 (1101-1250 ELO)", "#ffcd25"),
-        ("Level 5 (1251-1400 ELO)", "#ffcd25"),
-        ("Level 6 (1401-1550 ELO)", "#ffcd25"),
-        ("Level 7 (1551-1700 ELO)", "#ffcd25"),
-        ("Level 8 (1701-1850 ELO)", "#ff6c20"),
-        ("Level 9 (1851-2000 ELO)", "#ff6c20"),
-        ("Level 10 (2001+ ELO)", "#e80128"),
+    let required_roles: Vec<(&str, u32)> = vec![
+        ("Level 1 (1-800 ELO)", 0xDDDDDD),
+        ("Level 2 (801-950 ELO)", 0x47E36E),
+        ("Level 3 (951-1100 ELO)", 0x47E36E),
+        ("Level 4 (1101-1250 ELO)", 0xFFCD25),
+        ("Level 5 (1251-1400 ELO)", 0xFFCD25),
+        ("Level 6 (1401-1550 ELO)", 0xFFCD25),
+        ("Level 7 (1551-1700 ELO)", 0xFFCD25),
+        ("Level 8 (1701-1850 ELO)", 0xFF6C20),
+        ("Level 9 (1851-2000 ELO)", 0xFF6C20),
+        ("Level 10 (2001+ ELO)", 0xE80128),
     ];
 
     let roles: &HashMap<RoleId, Role> = &guild.roles;
@@ -79,22 +104,28 @@ async fn prepare_guild(ctx: Context, guild: Guild){
             info!("Found: key = {}, value = {}", key, value.name);
         } else {
             info!("Role '{}' not found, attempting to create!",role);
-            let builder = EditRole::new().name(role).colour(color).hoist(true).mentionable(true);
+
+            let builder = EditRole::new().name(role.to_string()).colour(Colour::new(*color)).hoist(true).mentionable(true);
             let new_role = guild.create_role(&ctx.http, builder).await;
-            if new_role.is_err() {
-                error!("Failed to create role in guild '{}'", guild.name);
-                return;
-            } else {
-                // There are better ways to make sure you don't hit rate limit.
-                // But we don't do that here.
-                sleep(Duration::from_millis(40)).await;
-                actual_roles.insert(*role, new_role.unwrap().id);
+
+            match new_role {
+                Ok(value) => {
+                    actual_roles.insert(*role, value.id);
+
+                    // There are better ways to make sure you don't hit rate limit.
+                    // But we don't do that here.
+                    sleep(Duration::from_millis(40)).await;
+                },
+                Err(e) => {
+                    error!("Failed to create role in guild '{}' reason: {}", guild.name, e)
+                    return None;
+                },
             }
-            return;
         }
     }
 
-    //guild.create_role()
-
     info!("Guild prepared!");
+
+    Some(actual_roles)
+
 }
